f4d9c91f7983f2ce74277a18410bfc1b5c23a1e1
ErikPrchlik <erikson006@gmail.com>
2021-03-23 19:09:34 +0100

    rezervácie

--- Adam-slim/src/routes-reservations.php
+++ Adam-slim/src/routes-reservations.php
@@ -5,5 +5,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/reservations', function (Request $request, Response $response, $args) {
 
-    return $this->view->render($response, 'reservations.latte');
+    try {
+        $stmt = $this->db->prepare('SELECT * FROM rezervacia
+                                    LEFT JOIN zakaznik USING (zakaznik_key)');
+        $stmt->execute();
+    } catch (Exception $ex) {
+        $this->logger->error($ex->getMessage());
+        die($ex->getMessage());
+    }
+
+    $tplVars['reservations'] = $stmt->fetchAll();
+
+    return $this->view->render($response, 'reservations.latte', $tplVars);
 })->setName('reservations');
