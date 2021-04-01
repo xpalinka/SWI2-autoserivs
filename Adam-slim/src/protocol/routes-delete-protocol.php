25f5b49b74b9a5f389628dd6c5364129620769e6
ErikPrchlik <erikson006@gmail.com>
2021-04-01 21:29:48 +0200

    Pridaný delete protokol

--- Adam-slim/src/protocol/routes-delete-protocol.php
+++ Adam-slim/src/protocol/routes-delete-protocol.php
@@ -4,4 +4,14 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->post('/delete-protocol', function (Request $request, Response $response, $args){
+    $id = $request->getQueryParam('id');
+    try{
+        $stmt = $this->db->prepare('DELETE FROM protokol WHERE protokol_key=:id');
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    } catch (Exception $ex){
+        $this->logger->error($ex->getMessage());
+        die($ex->getMessage());
+    }
+    return $response->withHeader('Location', $this->router->pathFor('protocols'));
 })->setName('delete-protocol');
