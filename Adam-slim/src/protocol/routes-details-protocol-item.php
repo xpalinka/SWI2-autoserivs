9b11714c3a187eb042b2b4250462536be7a056f7
ErikPrchlik <erikson006@gmail.com>
2021-04-01 21:38:18 +0200

    Príprava napolozku protokolu
    potrebná pre detail protokolu

---
+++ Adam-slim/src/protocol/routes-details-protocol-item.php
@@ -0,0 +1,8 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/details-protocol-item', function (Request $request, Response $response, $args) {
+
+})->setName('details-protocol-item');
