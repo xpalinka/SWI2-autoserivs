34646d41faa803b0fa5128f75a7fa60cafda3383
ErikPrchlik <erikson006@gmail.com>
2021-04-02 11:10:33 +0200

    Dorobenie create-user
    Treba prerobiť create-user.latte
    Prehodnotiť výber adresy, malo by to byť pripravené pre tú potrebu.

--- Adam-slim/src/routes-create-user.php
+++ Adam-slim/src/routes-create-user.php
@@ -5,28 +5,51 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/newWithAddress', function(Request $request, Response $response, $args) {
     $tplVars['form'] = ['ln' => '', 'fn' => '', 'rc' => '', 'r' => '', 'tf' => '', 't' => '', 'bu' => '', 'm' => '', 'ul' => '', 'oc' => '', 'pc' => '', 'zip' => '', 'st' => ''];
+    try {
+        $stmt = $this->db->prepare('SELECT * FROM pozicia');
+        $stmt->execute();
+    } catch (Exception $ex) {
+        $this->logger->error($ex->getMessage());
+        die($ex->getMessage());
+    }
+    $tplVars['pozicie'] = $stmt->fetchAll();
+
+    //keby chces vyberat z adres
+    try {
+        $stmt = $this->db->prepare('SELECT * FROM adresa');
+        $stmt->execute();
+    } catch (Exception $ex) {
+        $this->logger->error($ex->getMessage());
+        die($ex->getMessage());
+    }
+    $tplVars['adresy'] = $stmt->fetchAll();
+
     return $this->view->render($response, 'create_user.latte', $tplVars);
 })->setName('newWithAddress');
 
-$app->post('/add-person', function(Request $request, Response $response, $args) {
+$app->post('/newWithAddress', function(Request $request, Response $response, $args) {
     $data = $request->getParsedBody();  //$_POST
-    if(!empty($data['ln']) && !empty($data['fn']) && !empty($data['rc']) && !empty($data['r']) && !empty($data['bu']) && !empty($data['tf']) && !empty($data['m']) && !empty($data['t'])) {
+    if(!empty($data['ln']) && !empty($data['fn']) && !empty($data['rc']) && !empty($data['r']) && !empty($data['bu']) && !empty($data['tf']) && !empty($data['t']) && (!empty($data['adresa_key']) || !empty($data['t'])) ) {
         try {
             $this->db->beginTransaction();
 
-            $stmt=$this->db->prepare('INSERT INTO adresa
+            if(empty($data['adresa_key'])) {
+                $stmt = $this->db->prepare('INSERT INTO adresa
                                       (mesto, ulica, orientacne_cislo, popisne_cislo, psc, stat)
                                        VALUES 
                                       (:m, :ul, :oc, :oc, :pc, :zip, :st)');
-            $stmt->bindValue(':m', $data['m']);
-            $stmt->bindValue(':ul', $data['ul']);
-            $stmt->bindValue(':oc', $data['oc']==''?null:$data['oc']);
-            $stmt->bindValue(':pc', $data['pc']==''?null:$data['pc']);
-            $stmt->bindValue(':zip', $data['zip']);
-            $stmt->bindValue(':st', $data['st']);
-            $stmt->execute();
-            $idAddress=$this->db->lastInsertId('adresa_adresa_key_seq');
-
+                $stmt->bindValue(':m', $data['m']);
+                $stmt->bindValue(':ul', $data['ul']);
+                $stmt->bindValue(':oc', $data['oc'] == '' ? null : $data['oc']);
+                $stmt->bindValue(':pc', $data['pc'] == '' ? null : $data['pc']);
+                $stmt->bindValue(':zip', $data['zip']);
+                $stmt->bindValue(':st', $data['st']);
+                $stmt->execute();
+                $idAddress = $this->db->lastInsertId('adresa_adresa_key_seq');
+            } else {
+                //keby chces vyberat z adres
+                $idAddress = $data['adresa_key'];
+            }
 
             $stmt=$this->db->prepare('INSERT INTO zamestnanec
                                       (meno, prizvisko, rodne_cislo, email, heslo, bankovy_ucet, telefon, adresa_key, pozicia_key)
@@ -44,14 +67,6 @@ $app->post('/add-person', function(Request $request, Response $response, $args) 
             $stmt->bindValue(':poz', $data['t']);
             $stmt->execute();
 
-
-
-
-
-
-
-
-
             $this->db->commit();
 //            $tplVars['done'] = 'Operácia úspešná.';
         } catch (Exception $ex) {
@@ -59,7 +74,7 @@ $app->post('/add-person', function(Request $request, Response $response, $args) 
             if($ex->getCode() == 23505) {
                 $tplVars['error'] = 'Tento užívateľ už existuje.';
                 $tplVars['form'] = $data;
-                return $this->view->render($response, 'add-create_user.latte', $tplVars);
+                return $this->view->render($response, 'create_user.latte', $tplVars);
             } else {
                 $this->logger->error($ex->getMessage());
                 die($ex->getMessage());
