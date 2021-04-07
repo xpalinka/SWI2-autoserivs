cb6cd55e66b2310e8a8ba4394085e91917e5fb18
otradovec <otradovec@github.com>
2021-04-07 22:29:50 +0200

    Testing on dev, not on production

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,6 +1,6 @@
 describe('Login Test', () => {
   it('Visits login page', () => {
-    cy.visit('https://akela.mendelu.cz/~xpalinka/SWI2-autoservis/SWI2-autoserivs/Adam-slim/public/login')
+    cy.visit('https://akela.mendelu.cz/~xpalinka/SWI2-autoservis/test/SWI2-autoserivs/Adam-slim/public/login')
     cy.contains('Login')
     cy.contains('Heslo')
   })
