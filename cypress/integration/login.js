7d6789e5e94de41f9705f8c8ed7f1e0fefbcab3d
otradovec <otradovec@github.com>
2021-04-26 21:46:23 +0200

    Cypress - Login test bugfix

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -2,9 +2,9 @@ describe('Login Test', () => {
   it('Visits login page', () => {
     cy.visit('https://akela.mendelu.cz/~xpalinka/SWI2-autoservis/test/SWI2-autoserivs/Adam-slim/public/login')
     cy.contains('Login')
-    cy.get('input').type('xotradov@mendelu.cz')
+    cy.get('input').first().type('xotradov@mendelu.cz')
     cy.contains('Heslo')
-    cy.get('input').type('HESLO')
+    cy.get('input').last().type('HESLO')
     cy.get('form').submit()
     cy.get('Odhlasit').click()
     cy.visit('/')
