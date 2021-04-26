b462ec4c07caf4825f870fdcea64114c42323d8f
xlesakov <xlesakov@mendelu.cz>
2021-04-26 21:02:53 +0200

    create protocol from reservationn

--- Adam-slim/src/protocol/routes-create-protocol.php
+++ Adam-slim/src/protocol/routes-create-protocol.php
@@ -3,6 +3,6 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/create-protocol-item', function (Request $request, Response $response, $args) {
+$app->get('/create-protocol/', function (Request $request, Response $response, $args) {
 
-})->setName('details-protocol-item');
+})->setName('create-protocol');
