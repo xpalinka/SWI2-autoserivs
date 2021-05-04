4315cb1ca09851d2869da32e1d4b7030361785e1
ErikPrchlik <erikson006@gmail.com>
2021-05-04 19:04:30 +0200

    spravené pridávanie materiálu

--- Adam-slim/src/protocol/routes-details-protocol.php
+++ Adam-slim/src/protocol/routes-details-protocol.php
@@ -20,7 +20,7 @@ $app->get('/details-protocol', function (Request $request, Response $response, $
                                     LEFT JOIN zakaznik USING (zakaznik_key)
                                     
                                     LEFT JOIN (
-                                        SELECT protokol_key, COUNT(*) AS poc_poloziek, SUM(cena_za_pracu) AS cena_prace, 
+                                            SELECT protokol_key, COUNT(*) AS poc_poloziek, SUM(cena_za_pracu) AS cena_prace, 
                                                SUM(spotreba_materialu.mnozstvo * predajna_cena) AS cena_spotreby
                                         FROM polozka_protokolu
                                             
@@ -41,12 +41,21 @@ $app->get('/details-protocol', function (Request $request, Response $response, $
     }
     $tplVars['protocol'] = $stmt->fetchAll();
     try {
-        $stmt = $this->db->prepare('SELECT typ_opravy.nazov AS typ_opravy_nazov, material.nazov AS material_nazov, * FROM polozka_protokolu
+        $stmt = $this->db->prepare('SELECT typ_opravy.nazov AS typ_opravy_nazov, (cena_materialu_spolu + cena_za_pracu) AS celkova_cena_polozky, * FROM polozka_protokolu
                                     LEFT JOIN typ_opravy USING (typ_opravy_key)
-                                    LEFT JOIN spotreba_materialu USING (polozka_protokolu_key)
-                                    LEFT JOIN skladova_karta USING (skladova_karta_key)
-                                    LEFT JOIN material USING (material_key)
+                                    LEFT JOIN (SELECT polozka_protokolu_key, COUNT(*) AS poc_spotr, SUM((mnozstvo * predajna_cena)) AS cena_materialu_spolu FROM spotreba_materialu
+                                               LEFT JOIN skladova_karta USING (skladova_karta_key)
+                                               LEFT JOIN material USING (material_key)
+                                               GROUP BY polozka_protokolu_key
+                                    ) AS spot_mat
+                                    USING (polozka_protokolu_key)
                                     WHERE protokol_key = :id');
+//        $stmt = $this->db->prepare('SELECT typ_opravy.nazov AS typ_opravy_nazov, material.nazov AS material_nazov, * FROM polozka_protokolu
+//                                    LEFT JOIN typ_opravy USING (typ_opravy_key)
+//                                    LEFT JOIN spotreba_materialu USING (polozka_protokolu_key)
+//                                    LEFT JOIN skladova_karta USING (skladova_karta_key)
+//                                    LEFT JOIN material USING (material_key)
+//                                    WHERE protokol_key = :id');
         $stmt->bindValue(':id', $id);
         $stmt->execute();
     } catch (Exception $ex) {
