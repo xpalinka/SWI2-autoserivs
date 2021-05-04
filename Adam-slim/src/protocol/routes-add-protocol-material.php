123ae4fa45930860984ed6e37ae3e21a5a94182f
ErikPrchlik <erikson006@gmail.com>
2021-05-04 18:22:52 +0200

    Update routes-add-protocol-material.php

--- Adam-slim/src/protocol/routes-add-protocol-material.php
+++ Adam-slim/src/protocol/routes-add-protocol-material.php
@@ -39,42 +39,27 @@ $app->post('/add-protocol-material', function(Request $request, Response $respon
     if (true) {
         try {
             $this->db->beginTransaction();
+            try {
+                $stmt = $this->db->prepare("SELECT * FROM material
+                                            LEFT JOIN skladova_karta USING(material_key)
+                                            WHERE material_key = :id");
+                $stmt->bindValue(':id', $data['material_key']);
+                $stmt->execute();
+            } catch (Exception $ex) {
+                $this->logger->error($ex->getMessage());
+                die($ex->getMessage());
+            }
+            $material = $stmt->fetch();
+            $skladova_karta_key = $material['skladova_karta_key'];
 
-//            if(empty($data['adresa_key'])) {
-//                $stmt = $this->db->prepare('INSERT INTO adresa
-//                                      (mesto, ulica, orientacne_cislo, popisne_cislo, psc, stat)
-//                                       VALUES
-//                                      (:m, :ul, :oc, :pc, :zip, :st)');
-////                $stmt->bindValue(':id', );
-//                $stmt->bindValue(':m', $data['m']);
-//                $stmt->bindValue(':ul', $data['ul']);
-//                $stmt->bindValue(':oc', $data['oc'] == '' ? null : $data['oc']);
-//                $stmt->bindValue(':pc', $data['pc'] == '' ? null : $data['pc']);
-//                $stmt->bindValue(':zip', $data['zip']);
-//                $stmt->bindValue(':st', $data['st']);
-//                $stmt->execute();
-//                $idAddress = $this->db->lastInsertId('adresa_adresa_key_seq');
-//            } else {
-//                //keby chces vyberat z adres
-//                $idAddress = $data['adresa_key'];
-//            }
-//
-//            $stmt=$this->db->prepare('INSERT INTO zamestnanec
-//                                      (meno, priezvisko, rodne_cislo, email, heslo, bankovy_ucet, telefon, adresa_key, pozicia_key)
-//                                       VALUES
-//                                      (:fn, :ln, :rc, :r, :heslo, :bu, :tf, :adr, :poz)');
-//            $stmt->bindValue(':fn', $data['fn']);
-//            $stmt->bindValue(':ln', $data['ln']);
-//            $stmt->bindValue(':rc', $data['rc']);
-//            $stmt->bindValue(':r', $data['r']);
-//            $heslo = password_hash("HESLO", PASSWORD_DEFAULT);
-//            $stmt->bindValue(':heslo', $heslo);
-//            $stmt->bindValue(':bu', $data['bu']);
-//            $stmt->bindValue(':tf', $data['tf']);
-//            $stmt->bindValue(':adr', $idAddress);
-//            $stmt->bindValue(':poz', $data['t']);
-//            $stmt->execute();
-//
+            $stmt=$this->db->prepare('INSERT INTO spotreba_materialu
+                                      (mnozstvo, skladova_karta_key, polozka_protokolu_key)
+                                       VALUES
+                                      (:mnozstvo, :skladova_karta_key, :polozka_protokolu_key)');
+            $stmt->bindValue(':mnozstvo', $data['mnozstvo_materialu']);
+            $stmt->bindValue(':skladova_karta_key', $skladova_karta_key);
+            $stmt->bindValue(':polozka_protokolu_key', $data['polozka_protokolu_key']);
+            $stmt->execute();
             $this->db->commit();
 //            $tplVars['done'] = 'Operácia úspešná.';
         } catch (Exception $ex) {
@@ -83,6 +68,25 @@ $app->post('/add-protocol-material', function(Request $request, Response $respon
                 print $ex->getMessage();
                 $tplVars['error'] = 'Táto spotreba už existuje.';
                 $tplVars['form'] = $data;
+                try {
+                    $stmt = $this->db->prepare("SELECT * FROM material");
+                    $stmt->execute();
+                } catch (Exception $ex) {
+                    $this->logger->error($ex->getMessage());
+                    die($ex->getMessage());
+                }
+                $tplVars['materials'] = $stmt->fetchAll();
+                try {
+                    $stmt = $this->db->prepare("SELECT * FROM polozka_protokolu
+                                    LEFT JOIN typ_opravy USING(typ_opravy_key)
+                                    WHERE polozka_protokolu_key = :id");
+                    $stmt->bindValue(':id', $data['polozka_protokolu_key']);
+                    $stmt->execute();
+                } catch (Exception $ex) {
+                    $this->logger->error($ex->getMessage());
+                    die($ex->getMessage());
+                }
+                $tplVars['polozka'] = $stmt->fetch();
                 return $this->view->render($response, 'add-protocol-material.latte', $tplVars);
             } else {
                 $this->logger->error($ex->getMessage());
@@ -108,7 +112,7 @@ $app->post('/add-protocol-material', function(Request $request, Response $respon
             $stmt = $this->db->prepare("SELECT * FROM polozka_protokolu
                                     LEFT JOIN typ_opravy USING(typ_opravy_key)
                                     WHERE polozka_protokolu_key = :id");
-            $stmt->bindValue(':id', $id);
+            $stmt->bindValue(':id', $data['polozka_protokolu_key']);
             $stmt->execute();
         } catch (Exception $ex) {
             $this->logger->error($ex->getMessage());
