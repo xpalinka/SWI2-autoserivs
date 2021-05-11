b70d260fb03d5c55779667894195fc9737c674ae
Adam Malášek <xmalasek@mendelu.cz>
2021-05-11 15:54:46 +0200

    opravy_vol2

--- Adam-slim/src/protocol/routes-create-protocol.php
+++ Adam-slim/src/protocol/routes-create-protocol.php
@@ -42,3 +42,50 @@ $app->get('/create-protocol', function (Request $request, Response $response, $a
 
     return $this->view->render($response, 'create-protocol.latte',$tplVars);
 })->setName('create-protocol');
+
+$app->post('/create-user', function(Request $request, Response $response, $args) {
+    $data = $request->getParsedBody();  //$_POST
+    if(!empty($data['dv']) && !empty($data['pz']) && !empty($data['z']) && !empty($data['r']) ) {
+        try {
+            $this->db->beginTransaction();
+
+            $stmt=$this->db->prepare('INSERT INTO protokol
+                                      (datum_vystavenia, posledna_zmena, zamestnanec_key, rezervacia_key)                                       VALUES
+                                      (:dv, :pz, :z, :r)');
+            $stmt->bindValue(':fn', $data['fn']);
+            $stmt->bindValue(':ln', $data['ln']);
+            $stmt->bindValue(':rc', $data['rc']);
+            $stmt->bindValue(':r', $data['r']);
+
+            $stmt->execute();
+//
+            $this->db->commit();
+
+        } catch (Exception $ex) {
+            $this->db->rollback();
+            if($ex->getCode() == 23505) {
+                print $ex->getMessage();
+                $tplVars['error'] = 'Tento užívateľ už existuje.';
+                try {
+                    $stmt = $this->db->prepare('SELECT * FROM pozicia');
+                    $stmt->execute();
+                } catch (Exception $ex) {
+                    $this->logger->error($ex->getMessage());
+                    die($ex->getMessage());
+                }
+                $tplVars['pozicie'] = $stmt->fetchAll();
+                $tplVars['form'] = $data;
+                return $this->view->render($response, 'create-user.latte', $tplVars);
+            } else {
+                $this->logger->error($ex->getMessage());
+                die($ex->getMessage());
+            }
+        }
+        return $response->withHeader('Location', $this->router->pathFor('home'));
+    } else {
+        $tplVars['error'] = 'Nie sú vyplnené všetky údaje.';
+        $tplVars['form'] = $data;
+        return $this->view->render($response, 'create-user.latte', $tplVars);
+    }
+
+});
