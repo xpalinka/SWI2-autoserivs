cccbc8dd7a821ee7723fc4be211b10d1495783e0
Adam Malášek <xmalasek@mendelu.cz>
2021-05-15 19:01:50 +0200

    opravy_vol3

--- Adam-slim/src/protocol/routes-add-cinnost.php
+++ Adam-slim/src/protocol/routes-add-cinnost.php
@@ -16,4 +16,43 @@ $app->get('/add-cinnost', function(Request $request, Response $response, $args) 
 
         return $this->view->render($response, 'add-cinnost.latte',$tplVars);
 
+})->setName('add-cinnost');
+
+$app->post('/add-cinnost', function(Request $request, Response $response, $args) {
+    $data = $request->getParsedBody();  //$_POST
+    if(!empty($data['dv']) && !empty($data['pz']) && !empty($data['z']) && !empty($data['r']) ) {
+        try {
+            $this->db->beginTransaction();
+
+            $stmt=$this->db->prepare('INSERT INTO typ_opravy
+                                      (cena_za_pracu, nazov) 
+                                    VALUES
+                                      (:czp, :nc)');
+            $stmt->bindValue(':czp', $data['czp']);
+            $stmt->bindValue(':nc', $data['nc']);
+
+
+            $stmt->execute();
+//
+            $this->db->commit();
+
+        } catch (Exception $ex) {
+            $this->db->rollback();
+            if($ex->getCode() == 23505) {
+                print $ex->getMessage();
+                $tplVars['error'] = 'Tento prtokol už existsuje.';
+
+                return $this->view->render($response, 'add-cinnost.latte', $tplVars);
+            } else {
+                $this->logger->error($ex->getMessage());
+                die($ex->getMessage());
+            }
+        }
+        return $response->withHeader('Location', $this->router->pathFor('add-cinnost'));
+    } else {
+        $tplVars['error'] = 'Nie sú vyplnené všetky údaje.';
+        $tplVars['form'] = $data;
+        return $this->view->render($response, 'add-cinnost.latte', $tplVars);
+    }
+
 });
